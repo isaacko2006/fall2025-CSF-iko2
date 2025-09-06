bcff3fbf6a08676991a41a992649ae9ff51393cd
Gavin Simurdiak <gsimurd1@jhu.edu>
2025-09-06 19:14:41 -0400

    implemented fixpoint_mul, fixpoint_sub using add function, and began on format hex

--- csf_assign01/csf_assign01/fixpoint.c
+++ csf_assign01/csf_assign01/fixpoint.c
@@ -101,6 +101,7 @@ fixpoint_negate( fixpoint_t *val ) {
 result_t
 fixpoint_add( fixpoint_t *result, const fixpoint_t *left, const fixpoint_t *right ) {
   if (left->negative == right->negative) {
+    result->negative = left->negative;
    return addSameSign(result, left, right);
   } else {
     if (fixpoint_compare(left, right) == 1 || fixpoint_compare(left, right) == 0) {
@@ -116,11 +117,49 @@ fixpoint_add( fixpoint_t *result, const fixpoint_t *left, const fixpoint_t *righ
 result_t
 fixpoint_sub( fixpoint_t *result, const fixpoint_t *left, const fixpoint_t *right ) {
   // TODO: implement
+  fixpoint_t flippedRight = *right;
+  fixpoint_negate(&flippedRight);
+
+  return fixpoint_add(result, left, &flippedRight);
 }
 
 result_t
 fixpoint_mul( fixpoint_t *result, const fixpoint_t *left, const fixpoint_t *right ) {
   // TODO: implement
+  uint64_t leftMag = (((uint64_t)left->whole) << 32) | left->frac;
+  uint64_t rightMag = (((uint64_t)right->whole) << 32) | right->frac;
+
+  uint64_t Y = rightMag >> 32;
+  uint64_t Z = (uint32_t) rightMag;
+
+  uint64_t PRS = leftMag * Z;
+  uint64_t TUV = leftMag * Y;
+
+  uint64_t low32 = (uint32_t) PRS;
+  uint64_t mid64 = (PRS >> 32) + (TUV << 32);
+  uint64_t high32 = (TUV >> 32);
+
+  result->whole = (uint32_t)(mid64 >> 32);
+  result->frac = (uint32_t) mid64;
+
+  result->negative = (left->negative != right->negative);
+
+  if (result->whole == 0 && result->frac == 0 ) {
+    result->negative = false;
+  }
+
+  if (high32 != 0 && low32 != 0) {
+    return RESULT_OVERFLOW | RESULT_UNDERFLOW;
+  }
+  else if (high32 != 0) {
+    return RESULT_OVERFLOW;
+  }
+  else if (low32 != 0) {
+    return RESULT_UNDERFLOW;
+  }
+  else {
+    return RESULT_OK;
+  }
 }
 
 int
@@ -174,6 +213,13 @@ fixpoint_compare( const fixpoint_t *left, const fixpoint_t *right ) {
 void
 fixpoint_format_hex( fixpoint_str_t *s, const fixpoint_t *val ) {
   // TODO: implement
+  char arr[32];
+  char *arrPos = arr;
+
+  if (val->negative && (val->whole != 0 || val->frac != 0)) {
+    *arrPos++ = '-';
+  }
+  
 }
 
 bool

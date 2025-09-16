272b2fc0de6435ccc3037664083c682c5c199d51
Gavin Simurdiak <gsimurd1@jhu.edu>
2025-09-16 14:27:09 -0400

    initial implementation of emboss function and emboss_gray helper function

--- csf_assign02/c_imgproc_fns.c
+++ csf_assign02/c_imgproc_fns.c
@@ -5,6 +5,43 @@
 #include "imgproc.h"
 
 // TODO: define your helper functions here
+uint8_t emboss_gray(uint32_t pixel, uint32_t pixel_upperleft)
+{
+  //compute differences: diffr = nr-r, diffg = ng - g, diffb = nb - b
+  int32_t r = (pixel >> 24) & 0xFF;
+  int32_t g = (pixel >> 16) & 0xFF;
+  int32_t b = (pixel >> 8) & 0xFF;
+
+  int32_t nr = (pixel_upperleft >> 24) & 0xFF;
+  int32_t ng = (pixel_upperleft >> 16) & 0xFF;
+  int32_t nb = (pixel_upperleft >> 8) & 0xFF;
+
+  int32_t diffr = nr - r;
+  int32_t diffg = ng - g;
+  int32_t diffb = nb - b;
+
+  //priority - red,green,blue
+  int32_t diff = diffr;
+  if (abs(diffg) > abs(diff) || (abs(diffg) == abs(diff) && diff == diffr))
+  {
+    diff = diffg;
+  }
+  if (abs(diffb) > abs(diff) ||
+      (abs(diffb) == abs(diff) && diff != diffr && diff == diffg))
+  {
+    diff = diffb;
+  }
+
+  //gray is 128 + diff
+  int32_t gray = 128 + diff;
+  //must be clamped within range of 0-255
+  if (gray < 0)
+    gray = 0;
+  if (gray > 255)
+    gray = 255;
+
+  return (uint8_t)gray;
+}
 
 //! Transform the color component values in each input pixel
 //! by applying the bitwise complement operation. I.e., each bit
@@ -15,14 +52,16 @@
 //! @param input_img pointer to the input Image
 //! @param output_img pointer to the output Image (in which the
 //!                   transformed pixels should be stored)
-void imgproc_complement( struct Image *input_img, struct Image *output_img ) {
+void imgproc_complement(struct Image *input_img, struct Image *output_img)
+{
   uint32_t totalPixels = input_img->width * input_img->height;
-  
-  //parse through total pixels
-  for (int i = 0; i < totalPixels; i++) {
+
+  // parse through total pixels
+  for (int i = 0; i < totalPixels; i++)
+  {
     uint32_t pixel = input_img->data[i];
 
-    //isolate all the colors
+    // isolate all the colors
     uint8_t red = (pixel >> 24);
     uint8_t green = (pixel >> 16);
     uint8_t blue = (pixel >> 8);
@@ -32,9 +71,9 @@ void imgproc_complement( struct Image *input_img, struct Image *output_img ) {
     blue = ~blue;
     green = ~green;
 
-    //create output
-    output_img->data[i] = (((uint32_t)red << 24) |((uint32_t)green << 16) | 
-      ((uint32_t)blue << 8)  | (uint32_t)alpha);
+    // create output
+    output_img->data[i] = (((uint32_t)red << 24) | ((uint32_t)green << 16) |
+                           ((uint32_t)blue << 8) | (uint32_t)alpha);
   }
 }
 
@@ -52,17 +91,21 @@ void imgproc_complement( struct Image *input_img, struct Image *output_img ) {
 //! @return 1 if the transformation succeeded, or 0 if the
 //!         transformation can't be applied because the image
 //!         width and height are not the same
-int imgproc_transpose( struct Image *input_img, struct Image *output_img ) {
+int imgproc_transpose(struct Image *input_img, struct Image *output_img)
+{
   int height = input_img->height;
   int width = input_img->width;
   uint32_t totalPixels = input_img->width * input_img->height;
 
-  if (height != width) {
+  if (height != width)
+  {
     return 0;
   }
 
-  for (int i = 0; i < height; i ++) {
-    for (int j = 0; j < width; j++) {
+  for (int i = 0; i < height; i++)
+  {
+    for (int j = 0; j < width; j++)
+    {
       int inputIdx = i * width + j;
       int outputIdx = j * height + i;
       output_img->data[outputIdx] = input_img->data[inputIdx];
@@ -91,20 +134,26 @@ int imgproc_transpose( struct Image *input_img, struct Image *output_img ) {
 //! @param input_img pointer to the input Image
 //! @param output_img pointer to the output Image (in which the
 //!                   transformed pixels should be stored)
-void imgproc_ellipse( struct Image *input_img, struct Image *output_img ) {
+void imgproc_ellipse(struct Image *input_img, struct Image *output_img)
+{
   int height = input_img->height;
   int width = input_img->width;
   int a = width / 2;
   int b = height / 2;
 
-  for (int i = 0; i < height; i ++) {
-    for (int j = 0; j < width; j++) {
+  for (int i = 0; i < height; i++)
+  {
+    for (int j = 0; j < width; j++)
+    {
       int x = i - a;
       int y = j - b;
 
-      if ((10000*x*x)/(a*a)+(((10000*y*y)/(b*b)) < 10000)) {
+      if ((10000 * x * x) / (a * a) + (((10000 * y * y) / (b * b)) < 10000))
+      {
         output_img->data[i * width + height] = input_img->data[i * width + height];
-      } else {
+      }
+      else
+      {
         output_img->data[j * width + height] = 0xFF000000;
       }
     }
@@ -141,6 +190,39 @@ void imgproc_ellipse( struct Image *input_img, struct Image *output_img ) {
 //! @param input_img pointer to the input Image
 //! @param output_img pointer to the output Image (in which the
 //!                   transformed pixels should be stored)
-void imgproc_emboss( struct Image *input_img, struct Image *output_img ) {
+void imgproc_emboss(struct Image *input_img, struct Image *output_img)
+{
   // TODO: implement
+  int height = input_img->height;
+  int width = input_img->width;
+
+  for (int row = 0; row < height; row++)
+  {
+    for (int col = 0; col < width; col++)
+    {
+      //calculate index from flattened array
+      int idx = row * width + col;
+      uint32_t pixel = input_img->data[idx];
+      uint32_t a = pixel & 0xFF;
+
+      //checks for top row or left column
+      if (row == 0 || col == 0)
+      {
+        //make pixel gray
+        uint32_t gray = 128;
+        output_img->data[idx] = (gray << 24) | (gray << 16) | (gray << 8) | a;
+      }
+      else
+      {
+        //find top left neighboring index using flattened array calculation
+        int neighbor = (row - 1) * width + (col - 1);
+        uint32_t neighborpixel = input_img->data[neighbor];
+
+        //use helper function to find how much brighter/darker pixel should be compared to neighbor
+        uint8_t gray = emboss_gray(pixel, neighborpixel);
+        //create pixel with gray color from emboss_gray function
+        output_img->data[idx] = (gray << 24) | (gray << 16) | (gray << 8) | a;
+      }
+    }
+  }
 }

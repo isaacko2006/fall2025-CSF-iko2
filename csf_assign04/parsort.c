596b1beb69c320089125f6464813da43d17159ea
Gavin Simurdiak <gsimurd1@jhu.edu>
2025-11-08 15:22:58 -0500

    updated main function with code from instructions

--- csf_assign04/parsort.c
+++ csf_assign04/parsort.c
@@ -8,43 +8,77 @@
 #include <sys/mman.h>
 #include <sys/wait.h>
 
-int compare( const void *left, const void *right );
-void swap( int64_t *arr, unsigned long i, unsigned long j );
-unsigned long partition( int64_t *arr, unsigned long start, unsigned long end );
-int quicksort( int64_t *arr, unsigned long start, unsigned long end, unsigned long par_threshold );
+int compare(const void *left, const void *right);
+void swap(int64_t *arr, unsigned long i, unsigned long j);
+unsigned long partition(int64_t *arr, unsigned long start, unsigned long end);
+int quicksort(int64_t *arr, unsigned long start, unsigned long end, unsigned long par_threshold);
 
 // TODO: declare additional helper functions if needed
 
-int main( int argc, char **argv ) {
+int main(int argc, char **argv)
+{
   unsigned long par_threshold;
-  if ( argc != 3 || sscanf( argv[2], "%lu", &par_threshold ) != 1 ) {
-    fprintf( stderr, "Usage: parsort <file> <par threshold>\n" );
-    exit( 1 );
+  if (argc != 3 || sscanf(argv[2], "%lu", &par_threshold) != 1)
+  {
+    fprintf(stderr, "Usage: parsort <file> <par threshold>\n");
+    exit(1);
   }
 
   int fd;
 
   // open the named file
   // TODO: open the named file
+  fd = open(argv[1], O_RDWR);
+  if (fd < 0)
+  {
+    // file couldn't be opened: handle error and exit
+    perror("file couldn't be opened");
+    exit(1);
+  }
 
   // determine file size and number of elements
   unsigned long file_size, num_elements;
   // TODO: determine the file size and number of elements
+  struct stat statbuf;
+  int rc = fstat(fd, &statbuf);
+  if (rc != 0)
+  {
+    // handle fstat error and exit
+    perror("fstat error");
+    exit(1);
+  }
+  // statbuf.st_size indicates the number of bytes in the file
+  file_size = statbuf.st_size;
+  num_elements = file_size / sizeof(int64_t);
 
   // mmap the file data
   int64_t *arr;
   // TODO: mmap the file data
+  arr = mmap(NULL, file_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  close(fd); // file can be closed now
+  if (arr == MAP_FAILED)
+  {
+    // handle mmap error and exit
+    perror("mmap error");
+    exit(1);
+  }
+  // *arr now behaves like a standard array of int64_t.
+  // Be careful though! Going off the end of the array will
+  // silently extend the file, which can rapidly lead to
+  // disk space depletion!
 
   // Sort the data!
   int success;
-  success = quicksort( arr, 0, num_elements, par_threshold );
-  if ( !success ) {
-    fprintf( stderr, "Error: sorting failed\n" );
-    exit( 1 );
+  success = quicksort(arr, 0, num_elements, par_threshold);
+  if (!success)
+  {
+    fprintf(stderr, "Error: sorting failed\n");
+    exit(1);
   }
 
   // Unmap the file data
   // TODO: unmap the file data
+  munmap(arr, file_size);
 
   return 0;
 }
@@ -60,11 +94,12 @@ int main( int argc, char **argv ) {
 //   negative if *left < *right,
 //   positive if *left > *right,
 //   0 if *left == *right
-int compare( const void *left, const void *right ) {
+int compare(const void *left, const void *right)
+{
   int64_t left_elt = *(const int64_t *)left, right_elt = *(const int64_t *)right;
-  if ( left_elt < right_elt )
+  if (left_elt < right_elt)
     return -1;
-  else if ( left_elt > right_elt )
+  else if (left_elt > right_elt)
     return 1;
   else
     return 0;
@@ -76,7 +111,8 @@ int compare( const void *left, const void *right ) {
 //   arr - pointer to first element of array
 //   i - index of element to swap
 //   j - index of other element to swap
-void swap( int64_t *arr, unsigned long i, unsigned long j ) {
+void swap(int64_t *arr, unsigned long i, unsigned long j)
+{
   int64_t tmp = arr[i];
   arr[i] = arr[j];
   arr[j] = tmp;
@@ -95,34 +131,38 @@ void swap( int64_t *arr, unsigned long i, unsigned long j ) {
 //   all elements to the left of the pivot will have values less than
 //   the pivot element, and all elements to the right of the pivot will
 //   have values greater than or equal to the pivot
-unsigned long partition( int64_t *arr, unsigned long start, unsigned long end ) {
-  assert( end > start );
+unsigned long partition(int64_t *arr, unsigned long start, unsigned long end)
+{
+  assert(end > start);
 
   // choose the middle element as the pivot
   unsigned long len = end - start;
-  assert( len >= 2 );
+  assert(len >= 2);
   unsigned long pivot_index = start + (len / 2);
   int64_t pivot_val = arr[pivot_index];
 
   // stash the pivot at the end of the sequence
-  swap( arr, pivot_index, end - 1 );
+  swap(arr, pivot_index, end - 1);
 
   // partition all of the elements based on how they compare
   // to the pivot element: elements less than the pivot element
   // should be in the left partition, elements greater than or
   // equal to the pivot should go in the right partition
   unsigned long left_index = start,
-                right_index = start + ( len - 2 );
+                right_index = start + (len - 2);
 
-  while ( left_index <= right_index ) {
+  while (left_index <= right_index)
+  {
     // extend the left partition?
-    if ( arr[left_index] < pivot_val ) {
+    if (arr[left_index] < pivot_val)
+    {
       ++left_index;
       continue;
     }
 
     // extend the right partition?
-    if ( arr[right_index] >= pivot_val ) {
+    if (arr[right_index] >= pivot_val)
+    {
       --right_index;
       continue;
     }
@@ -130,14 +170,14 @@ unsigned long partition( int64_t *arr, unsigned long start, unsigned long end ) 
     // left_index refers to an element that should be in the right
     // partition, and right_index refers to an element that should
     // be in the left partition, so swap them
-    swap( arr, left_index, right_index );
+    swap(arr, left_index, right_index);
   }
 
   // at this point, left_index points to the first element
   // in the right partition, so place the pivot element there
   // and return the left index, since that's where the pivot
   // element is now
-  swap( arr, left_index, end - 1 );
+  swap(arr, left_index, end - 1);
   return left_index;
 }
 
@@ -156,30 +196,32 @@ unsigned long partition( int64_t *arr, unsigned long start, unsigned long end ) 
 //
 // Return:
 //   1 if the sort was successful, 0 otherwise
-int quicksort( int64_t *arr, unsigned long start, unsigned long end, unsigned long par_threshold ) {
-  assert( end >= start );
+int quicksort(int64_t *arr, unsigned long start, unsigned long end, unsigned long par_threshold)
+{
+  assert(end >= start);
   unsigned long len = end - start;
 
   // Base case: if there are fewer than 2 elements to sort,
   // do nothing
-  if ( len < 2 )
+  if (len < 2)
     return 1;
 
   // Base case: if number of elements is less than or equal to
   // the threshold, sort sequentially using qsort
-  if ( len <= par_threshold ) {
-    qsort( arr + start, len, sizeof(int64_t), compare );
+  if (len <= par_threshold)
+  {
+    qsort(arr + start, len, sizeof(int64_t), compare);
     return 1;
   }
 
   // Partition
-  unsigned long mid = partition( arr, start, end );
+  unsigned long mid = partition(arr, start, end);
 
   // Recursively sort the left and right partitions
   int left_success, right_success;
   // TODO: modify this code so that the recursive calls execute in child processes
-  left_success = quicksort( arr, start, mid, par_threshold );
-  right_success = quicksort( arr, mid + 1, end, par_threshold );
+  left_success = quicksort(arr, start, mid, par_threshold);
+  right_success = quicksort(arr, mid + 1, end, par_threshold);
 
   return left_success && right_success;
 }
